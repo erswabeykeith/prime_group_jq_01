4dc8f218c7ec0cdecc6f9d8912e90760212cdd32
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-03 12:48:05 -0500

    Thurs am attempt

--- script.js
+++ script.js
@@ -1,30 +1,93 @@
 $(document).ready(function() {
 	var totalCash = 100;
 
-	var applePrice = randomNumber(-50, 50);
-	var orangePrice	= randomNumber(-50, 50);
-	var bananaPrice = randomNumber(-50, 50);
-	var grapesPrice	= randomNumber(-50, 50);
+	var applePrice = 5;
+	var orangePrice = 5;
+	var bananaPrice = 5;
+	var pearPrice = 5;
 
-	setInterval(function () {
-		applePrice = randomNumber(-50, 50);
-		orangePrice	= randomNumber(-50, 50);
-		bananaPrice = randomNumber(-50, 50);
-		grapesPrice	= randomNumber(-50, 50);
-		applePrice /= 100;
-		orangePrice /= 100;
-		bananaPrice /= 100;
-		grapesPrice /= 100;
+	var appleInventory = 0;
+	var orangeInventory = 0;
+	var bananaInventory = 0;
+	var pearInventory = 0;
 
-		$("h1").text("Total Available Cash: $" + totalCash);
+	appendToDom();
+	function appendToDom() {
 
-		$("#priceApple").text("Average Price: $" + applePrice);
-		$("#priceOrange").text("Average Price: $" + orangePrice);
-		$("#priceBanana").text("Average Price: $" + bananaPrice);
-		$("#priceGrapes").text("Average Price: $" + grapesPrice);
+	$("h1").text("Total Available Cash: $" + totalCash);
+	$("#priceApple").text("Average Price: $" + applePrice);
+	$("#priceOrange").text("Average Price: $" + orangePrice);
+	$("#priceBanana").text("Average Price: $" + bananaPrice);
+	$("#pricePear").text("Average Price: $" + pearPrice);
+  }
+	setInterval(function priceChange() {
+		var applePriceChange = randomNumber(-50, 50);
+		var orangePriceChange	= randomNumber(-50, 50);
+		var bananaPriceChange = randomNumber(-50, 50);
+		var pearPriceChange	= randomNumber(-50, 50);
 
-		console.log(applePrice, orangePrice, bananaPrice, grapesPrice)
-	}, 5000);
+		applePrice -= Math.round(applePriceChange) / 100;
+		orangePrice -= Math.round(orangePriceChange) / 100;
+		bananaPrice -= Math.round(bananaPriceChange) / 100;
+		pearPrice -= Math.round(pearPriceChange) / 100;
+
+		applePrice = parseFloat(applePrice.toFixed(2));
+			if (applePrice < .50) {
+				applePrice = .5
+			} else if (applePrice > 9.99) {
+				applePrice = 9.99
+			}
+		orangePrice = parseFloat(orangePrice.toFixed(2));
+		if (orangePrice < .50) {
+			orangePrice = .5
+		} else if (orangePrice > 9.99) {
+			orangePrice = 9.99
+		}
+		bananaPrice = parseFloat(bananaPrice.toFixed(2));
+		if (bananaPrice < .50) {
+			bananaPrice = .5
+		} else if (bananaPrice > 9.99) {
+			bananaPrice = 9.99
+		}
+		pearPrice = parseFloat(pearPrice.toFixed(2));
+		if (pearPrice < .50) {
+			pearPrice = .5
+		} else if (pearPrice > 9.99) {
+			pearPrice = 9.99
+		}
+
+		console.log(appleInventory);
+
+
+		appendToDom();
+
+		// console.log(applePrice, orangePrice, bananaPrice, pearPrice)
+	}, 1000);
+
+	$('button').on('click', function(){
+		appleInventory++;
+		totalCash -= applePrice;
+		$(this).parent().find('h4').html(appleInventory);
+	});
+
+	$('button').on('click', function(){
+		orangeInventory++;
+		totalCash -= orangePrice;
+		$(this).parent().find('h4').html(orangeInventory);
+	});
+
+	$('button').on('click', function(){
+		bananaInventory++;
+		totalCash -= bananaPrice;
+		$(this).parent().find('h4').html(bananaInventory);
+	});
+
+	$('button').on('click', function(){
+		pearInventory++;
+		totalCash -= pearPrice;
+		$(this).parent().find('h4').html(pearInventory);
+	});
+
 
 
 });
